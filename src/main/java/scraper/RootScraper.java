b44b54e46b3c7e303e9c3763c43edac7e265ac81
Robbie Fishel <robbie.fishel6@gmail.com>
2022-07-23 00:58:56 -0400

    Added code to functionally make team info fetching useful

--- src/main/java/scraper/RootScraper.java
+++ src/main/java/scraper/RootScraper.java
@@ -1,12 +1,9 @@
 package scraper;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import model.ModelElement;
-
-import org.jsoup.Jsoup;
 
 public class RootScraper extends SpotracScraper {
 
@@ -16,18 +13,14 @@ public class RootScraper extends SpotracScraper {
 
     @Override
     public List<SpotracScraper> scrapeTableForSubLinks() {
-        try {
-            return Jsoup.connect(super.getUrl()).get()
-                .selectFirst(".teams")
-                .selectFirst("tbody")
-                .select("tr")
-                .select("a")
-                .stream().map(teamLinkElement -> {
-                    return new TeamScraper(teamLinkElement.attr("href"));
-                }).collect(Collectors.toList());
-        } catch (IOException e) {
-            return null;
-        }
+        return super.getDocument()
+            .selectFirst(".teams")
+            .selectFirst("tbody")
+            .select("tr")
+            .select("a")
+            .stream().map(teamLinkElement -> {
+                return new TeamScraper(teamLinkElement.attr("href"));
+            }).collect(Collectors.toList());
     }
 
     @Override
--- src/main/java/scraper/TeamScraper.java
+++ src/main/java/scraper/TeamScraper.java
@@ -1,6 +1,5 @@
 package scraper;
 
-import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.stream.Collectors;
@@ -8,57 +7,45 @@ import java.util.stream.Collectors;
 import model.ModelElement;
 import model.Team;
 
-import org.jsoup.Jsoup;
-import org.jsoup.nodes.Document;
 import org.jsoup.nodes.Element;
 
 public class TeamScraper extends SpotracScraper {
+    private String teamID;
+
     public TeamScraper(String url) {
         super(url);
+        this.teamID = super.getDocument().location().split("/")
+            [super.getDocument().location().split("/").length-2];
     }
 
     @Override
     public List<SpotracScraper> scrapeTableForSubLinks() {
-        try {
-            return Jsoup.connect(super.getUrl()).get()
-                .selectFirst(".teams")
-                .selectFirst("tbody")
-                .select("tr")
-                .select("a")
-                .stream().map(teamLinkElement -> {
-                    return new PlayerScraper(teamLinkElement.attr("href"));
-                }).collect(Collectors.toList());
-        } catch (IOException e) {
-            return null;
-        }
+        return super.getDocument()
+            .selectFirst(".teams")
+            .selectFirst("tbody")
+            .select("tr")
+            .select("a")
+            .stream().map(teamLinkElement -> {
+                return new PlayerScraper(teamLinkElement.attr("href"), this.teamID);
+            }).collect(Collectors.toList());
     }
 
     @Override
     public ModelElement getInfo() {
-        try {
-            Document document = Jsoup.connect(super.getUrl()).get();
-
-            String redirectUrl = document.location();
-            String[] redirectUrlArgs = redirectUrl.split("/");
-            String teamID = redirectUrlArgs[redirectUrlArgs.length-2];
-
-            Optional<Element> keywords = document.select("meta")
-                .stream().filter(metaEle -> metaEle.attr("name").equals("keywords")).findFirst();
-            if (!keywords.isPresent()) {
-                return null;
-            }
-            String[] content = keywords.get().attr("content").split(", ");
-            String city = content[2];
-            String name = content[3];
-
-            int payroll = 0; // implement
-            return new Team(
-                teamID,
-                city,
-                name,
-                payroll);
-        } catch (IOException e) {
+        Optional<Element> keywords = super.getDocument().select("meta")
+            .stream().filter(metaEle -> metaEle.attr("name").equals("keywords")).findFirst();
+        if (!keywords.isPresent()) {
             return null;
         }
+        String[] content = keywords.get().attr("content").split(", ");
+        String city = content[2];
+        String name = content[3];
+
+        int payroll = 0; // implement
+        return new Team(
+            this.teamID,
+            city,
+            name,
+            payroll);
     }
 }
